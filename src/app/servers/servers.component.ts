24055daa0d52aee1b2db8b149fcab10272e80269
Faysal Regal <faysalregal@gmail.com>
2022-08-07 20:40:20 +0100

    Added prop binding and string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ServersComponent {
+  allowNewServer = false;
+  
+  //Constructor is the function that runs as soon as Angular App is spun up.
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
-
 }
